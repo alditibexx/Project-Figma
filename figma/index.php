324875f3de07cca1f3daa62315339c2d6cbd4d2b
Aldi Yudha Pramudia <67263058+alditibexx@users.noreply.github.com>
2022-12-28 10:57:53 +0700

    Add files via upload

---
+++ figma/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FIGMA</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    </head>
+    <body>  
+    <div class="container">
+      <div class="bg-primary">
+    <h2 class="text-center text-white">Saya Seorang Front End</h2>
+    <h4 class="text-center card-mt100 text-white">Sejak 1 Tahun lalu saya memulai prigramming</h4>
+    <a href ="#" class="btn btn-warning card-mt6" style="margin-left: 230px;">Pelajari</a>
+    <!-- header card -->
+    
+  <div class="card-header bg-primary text-white">
+    
+
+  <div class="card-body">
+    
+    </form>
+  </div>
+</div>
+</div>
+    <!-- Footer Card -->
+
+    <!-- Header Card Tabel -->
+    <div class="card mt-3">
+  <div class="card-header text-black text-center">
+  <h3>Profile</h3>
+  <h5>Mengenal Saya Secara Singkat</h5>
+
+  </div>
+  <div class="card-body">
+    <table class="table table-bordered table-striped">
+      
+<p>Aliquam blandit ice lorem</p>
+    </table>
+  </div>
+</div>
+</div>
+    <!-- Footer Card Tabel-->
+
+    <script type="text/javascript" src="js/bootstrap.min.css"></script>
+    </body>
+  </div>
+</html>
